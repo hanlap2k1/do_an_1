94226404c466f07b6f2733dcb6ee6514ab1090e3
hanlap2k1 <nrogamerac123@example.com>
2023-05-08 17:16:51 +0700

    first commit

--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 const app = express();
-const port = 80;
+const port = 3000;
 
 import {route} from './routes/index.js';
 
